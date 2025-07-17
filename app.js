98efa795c56b646f3ed4a84d0a45532fb2b20b9c
Shivam <mailshiivam@gmail.com>
2025-07-18 00:23:46 +0530

    Implemented image upload using Multer for homes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const multer=require('multer');
 require("dotenv").config();
 const DB_PATH = process.env.MONGO_URI
 
@@ -26,7 +27,43 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
+const randomString = (length) => {
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, randomString(10) + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const multerOptions = {
+  storage, fileFilter
+};
+
 app.use(express.urlencoded());
+app.use(multer(multerOptions).single('photo'));
+app.use(express.static(path.join(rootDir, 'public')))
+app.use("/uploads",express.static(path.join(rootDir, 'uploads')))
+app.use("/host/uploads/",express.static(path.join(rootDir, 'uploads')))
+app.use("/homes/uploads/",express.static(path.join(rootDir, 'uploads')))
+
 app.use(session({
   secret:"shivams Secret",//random value will be replaced later
   resave: false,
@@ -49,8 +86,6 @@ app.use("/host", (req, res, next) => {
   }
 });
 app.use("/host", hostRouter);
-
-app.use(express.static(path.join(rootDir, 'public')))
 
 app.use(errorsController.pageNotFound);
 
